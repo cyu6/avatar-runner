26d00f40647b6eb84c3cb2d8a862b23c0b9ab822
lil-z <llzhang@princeton.edu>
2020-05-03 15:29:24 -0400

    smooth movement and game pov

--- src/components/objects/Avatar/Avatar.js
+++ src/components/objects/Avatar/Avatar.js
@@ -1,26 +1,34 @@
-import { Group, Clock} from 'three';
-import { GLTFLoader} from 'three/examples/jsm/loaders/GLTFLoader.js';
-import {AnimationMixer} from 'three/src/animation/AnimationMixer.js';
+import { Group, Clock, Vector3 } from 'three';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+import { AnimationMixer } from 'three/src/animation/AnimationMixer.js';
+import { TWEEN } from 'three/examples/jsm/libs/tween.module.min.js';
 import MODEL from './avatar.gltf';
 
-class Avatar extends Group{
+
+class Avatar extends Group {
     constructor(parent) {
         // Call parent Group() constructor
         super();
 
         // set position of runner (increasing z moves runner closer to viewer)
-        this.position.set(0, 0, 1);
+        this.state = {
+            move: false,
+            left: false,
+            right: false,
+            speed: 0,
+        }
+        this.position.set(0, 0, 6);
         this.name = 'avatar';
         this.mixer;
 
         this.clock = new Clock();
         const loader = new GLTFLoader();
         loader.load(MODEL, (gltf) => {
-        	this.mixer = new AnimationMixer(gltf.scene);
+            this.mixer = new AnimationMixer(gltf.scene);
             gltf.scene.rotation.y = 3;
-        	this.mixer.timescale = 0.01
-        	var action = this.mixer.clipAction(gltf.animations[0]);
-        	action.play();
+            this.mixer.timescale = 0.01
+            var action = this.mixer.clipAction(gltf.animations[0]);
+            action.play();
 
             this.add(gltf.scene);
         });
@@ -28,9 +36,31 @@ class Avatar extends Group{
         parent.addToUpdateList(this);
     }
 
+    moveAvatar() {
+        if (this.move == true) {
+            if (this.left == true) {
+                if (this.position.x == -2.5) return;
+                let targetx = this.position.x - 2;
+                if (targetx < -2.5) targetx = -2.5;
+            
+                const goLeft = new TWEEN.Tween(this.position).to({ x: targetx }, 1000).easing(TWEEN.Easing.Quadratic.Out);
+                goLeft.start();
+            }
+            else if (this.right == true) {
+                if (this.position.x == 2.5) return;
+                let targetx = this.position.x + 2;
+                if (targetx > 2.5) targetx = 2.5;
+                const goRight = new TWEEN.Tween(this.position).to({ x: targetx }, 1000).easing(TWEEN.Easing.Quadratic.Out);
+                goRight.start();
+            }
+        }
+    }
+
     update() {
         var delta = this.clock.getDelta();
         if (this.mixer) this.mixer.update(delta);
+
+        TWEEN.update();
     }
 }
 
